83921ce9b82e355aa239182400b420cb697309dc
zeroth <frangmc@yahoo.es>
2020-12-26 01:14:34 +0100

    added p1_6_2v

--- ch1/p1_6_2/cpp/p1_6_2v.cpp
+++ ch1/p1_6_2/cpp/p1_6_2v.cpp
@@ -1,37 +1,38 @@
 /*
  * Minesweeper
  *
- * using C-style arrays
+ * using C++ vector
  * */
 
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <vector>
 
 #define MINE '*'
 
-int countMines(int r, int c, int i, int j, char *b) {
+int countMines(int i, int j, const std::vector<std::vector<char>>& b) {
     int count = 0;
     int r_ini = (i > 0) ? i - 1 : i;
-    int r_end = (i < r - 1) ? i + 1 : i;
+    int r_end = (i < b.size() - 1) ? i + 1 : i;
     int c_ini = (j > 0) ? j - 1 : j;
-    int c_end = (j < c - 1) ? j + 1 : j;
+    int c_end = (j < b[0].size() - 1) ? j + 1 : j;
     for (int m = r_ini; m <= r_end; m++)
         for (int n = c_ini; n <= c_end; n++)
-            if (*((b + m * c) + n) == MINE) ++count;
+            if (b[m][n] == MINE) ++count;
     return count;
 }
 
-void printField(int r, int c, int f, char *b) {
+void printField(int f, const std::vector<std::vector<char>>& b) {
     if (f > 1) std::cout << std::endl;
     std::cout << "Field #" << f << ":\n";
 
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++)
-            if (*((b + i * c) + j) == MINE)
+    for (int i = 0; i < b.size(); i++) {
+        for (int j = 0; j < b[0].size(); j++)
+            if (b[i][j] == MINE)
                 std::cout << MINE;
             else
-                std::cout << countMines(r, c, i, j, b);
+                std::cout << countMines(i, j, b);
         std::cout << std::endl;
     }
 }
@@ -44,14 +45,18 @@ int main() {
         std::cin >> r >> c;
         if (r == 0 && c == 0) break;
 
-        char board[r][c], ch;
+        std::vector<std::vector<char>> board(r, std::vector<char>(c));
+
+        char ch;
         std::cin.get(ch);  // skip endl
+
         for (int i = 0; i < r; i++) {
-            std::cin.read(board[i], c);
+            for(int j=0; j<c; j++) 
+                std::cin.get(board[i][j]);
             std::cin.get(ch);  // skip endl
         }
 
-        printField(r, c, ++f, (char *)board);
+        printField(++f, board);
     }
 
     return EXIT_SUCCESS;
